c50a98eea2b69843eccc47054435664549c23cb9
Kazi Sohrab Uddin <kazisohrab73@gmail.com>
2023-02-22 02:54:23 +0600

    updating

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors:{ 
+        "iub-color-primary": "#7f1d1d",
+      }
+    },
+    fontFamily:{
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container:{
+      padding:"2rem",
+      center:true,
+    },
+    screens:{
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+}
