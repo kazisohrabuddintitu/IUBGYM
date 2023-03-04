b7c55468ac5461585d3c903e0484106fddadfa2c
Kazi Sohrab Uddin <kazisohrab73@gmail.com>
2023-03-05 01:51:47 +0600

    updating

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,47 +11,92 @@
       </div>
     </div>
 
-    <nav class=" bg-gradient-to-r from-iub-color-primary to-nav-color px-2 sm:px-4 py-2.5 ">
-      <div class="container-fluid flex flex-wrap justify-between mx-auto">    
-          <span class="italic hover:not-italic self-center text-white font-thin">IUB GYM</span>     
-          
-          <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-          </button>
-          
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="flex flex-col p-4 mt-4 border bg-nav-color border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-nav-color dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-              </li>
-              <li>
-                <a href="/membership" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Membership</a>
-              </li>
-              <li>
-                <a href="/notice" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Notice Board</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-              </li>
-            </ul>
-          </div>      
-      </div>
-    </nav>
+    <nav
+        class="
+          md:flex md:justify-between md:items-center
+          bg-gradient-to-r from-iub-color-primary to-nav-color px-2 sm:px-4 py-2.5
+        "
+      >
+        <div class="flex items-center justify-between">
+          <router-link
+            to="/"
+            class="
+              text-xl 
+              text-white
+              md:text-2xl
+              italic hover:not-italic
+            "
+            >IUB GYM
+          </router-link>
+          <!-- Mobile menu button -->
+          <div @click="showMenu = !showMenu" class="flex md:hidden">
+            <button
+              type="button"
+              class="
+                text-gray-800
+                hover:text-gray-400
+                focus:outline-none focus:text-gray-400
+              "
+            >
+              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                <path
+                  fill-rule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
 
-    <div>
 
-    </div>
-
-
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <ul
+          :class="showMenu ? 'flex' : 'hidden'"
+          class="
+            flex-col
+            mt-8
+            space-y-4
+            md:flex
+            md:space-y-0
+            md:flex-row
+            md:items-center
+            md:space-x-10
+            md:mt-0
+            
+          "
+        >
+          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+            <a href="javascript:window.location.reload(true)" class="">Home</a>
+          </li>
+          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+            <a href="javascript:window.location.reload(true)"> About</a>  
+          </li>
+          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+            <a href="/membership">Membership</a>
+            
+          </li>
+          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+            <a href="/notice">Notice Board</a>
+            
+          </li>
+          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+            <a href="javascript:window.location.reload(true)">Contact Us</a>
+            
+          </li>
+        </ul>
+      </nav>
   </main>
-    
+
 </template>
 
-<script setup>
+<script>
 import {ref} from "vue";
 
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+};
 </script>
